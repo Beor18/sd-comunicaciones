4c02ca476a4234b031ec1c40991ae47f08b083af
Fernando Lopez <fer.ragnar19@gmail.com>
2017-06-13 20:28:41 -0300

    Agregado contacto

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'info@sd-comunicaciones.com';
+$subject = 'Mensaje de formulario '.$field_name;
+
+$body_message = 'Desde: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Mensaje: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('MENSAJE ENVIADO!! En breve nos pondremos en contacto....');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Error al enviar. Por Favor, envie un email a info@sd-comunicaciones.com');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+?>
